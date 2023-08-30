43104678f1c63522226d0d4402ecfe96ae136772
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-08-29 21:56:17 -0300

    creating project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+import styles from './header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        
+        <h2 className={styles.header_title}> Meu Controle Financeiro </h2>
+
+    </header>
+  )
+}
+
+export default Header
